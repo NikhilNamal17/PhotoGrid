a8a4ed46a57c7d19ee0fd9920fbef007d6779c19
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-06-21 23:44:46 +0530

    bugs solved

--- src/scripts/wallpaerLoad.js
+++ src/scripts/wallpaerLoad.js
@@ -1,29 +1,32 @@
-import wallpaperExE from "wallpaper/lib/win-wallpaper.exe";
-import os from "os";
-import wallpaper from "wallpaper";
-import fs from "fs";
-import path from "path";
-
-var self = this;
+const wallpaperExE = require("wallpaper/source/win-wallpaper.exe");
+const os = window.require("os");
+let wallpaper = window.require("wallpaper");
+const path = window.require("path");
+const fs = window.require("fs");
 
 let convertToBase64 = img => {
     let canvas = document.createElement("canvas");
-    canvas.width = window.screen.width;
-    canvas.height = window.screen.height;
+    canvas.width = 1080;
+    canvas.height = window.screen.availHeight;
     let context = canvas.getContext("2d");
     context.drawImage(img, 0, 0);
-    let dataURL = canvas.toDataURL("image/jpg");
+    let dataURL = canvas.toDataURL("image/png");
+
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 };
 
 let setAsBackground = event => {
-    var img = event.target;
-    img.setAttribute("crossOrigin", "anonymous");
-    img.src = event.target.url;
-    let base64Image = convertToBase64(img);
-    let picturePath = path.join(os.homedir(), "/Pictures", "background.jpg");
+    console.log("Event hai ye -> " + event.target.src);
+    var image = event.target;
+    image.setAttribute("crossOrigin", "anonymous");
+    image.src = event.target.src;
+    let base64Image = convertToBase64(image);
+    let picturePath = path.join(os.homedir(), "/Pictures", "bg.jpg");
+    console.log(picturePath);
     picturePath = path.normalize(picturePath);
     fs.writeFile(picturePath, base64Image, "base64", err => {
+        console.log("Picture path" + picturePath);
+
         wallpaper.set(picturePath, { scale: "stretch" }).then(() => {
             console.log(path.resolve(picturePath));
             console.log("DOne!");
