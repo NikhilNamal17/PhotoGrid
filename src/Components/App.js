080cad2f55776143917d9f11639f69ef01a13a79
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-08-23 21:43:33 +0530

    new build

--- src/Components/App.js
+++ src/Components/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
             }
         });
         this.setState({ images: response.data.results });
+        console.log(response.data.results);
     }
 
     async onDefault() {
