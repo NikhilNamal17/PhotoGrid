52e7f7d2365e899df847af8a0e20f9a1d08d0e51
NikhilNamal17 <nikhilnamal16@gmail.com>
2019-08-23 21:43:56 +0530

    new build

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -8,36 +8,60 @@ class ImageCard extends React.Component {
 
         this.imageRef = React.createRef();
         this.state = {
-            spans: 0
+            spans: 0,
+            height: 0
         };
     }
-    // state = {
-    //     convertToBase64: wall_paper.methods.convertToBase64,
-    //     setAsBackground: wall_paper.methods.setAsBackground
-    // };
+
     componentDidMount() {
         this.imageRef.current.addEventListener("load", this.setSpans);
     }
 
     setSpans = () => {
         const height = this.imageRef.current.clientHeight;
-        const spans = Math.ceil(height / 10 + 1);
+        const spans = Math.ceil(height / 10 + 12);
 
         this.setState({ spans: spans });
     };
 
     render() {
-        const { urls, alt_description } = this.props.image;
+        const { urls, alt_description, user } = this.props.image;
 
         return (
             <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
-                <img
+                {/* <img
                     className="image"
                     ref={this.imageRef}
                     src={urls.regular}
                     alt={alt_description}
                     onClick={wall_paper.setAsBackground}
-                />
+                /> */}
+                {/* <div>
+                    <h3>
+                        Photo By: {user.first_name + user.last_name} on{" "}
+                        <a href={user.links.html}>Unsplash</a>{" "}
+                    </h3>
+                </div> */}
+
+                <div className="ui card image-nik">
+                    <div className="image">
+                        <img
+                            // className="image-nik"
+                            ref={this.imageRef}
+                            src={urls.regular}
+                            alt={alt_description}
+                            onClick={wall_paper.setAsBackground}
+                        />
+                    </div>
+                    <div className="content">
+                        <a className="header">
+                            Photo By: {user.first_name + user.last_name} on
+                            <a href={user.links.html}> Unsplash</a>
+                        </a>
+
+                        <div class="description">{alt_description}</div>
+                    </div>
+                </div>
             </div>
         );
     }
